b548c200e99c721c982b8541459cd7ee0034ed74
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-05-11 18:22:52 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,24 +64,25 @@ for await (const req of s) {
         </div>
         <div className="max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8 mt-20">
           <p className="my-4 text-gray-700">
-            Deno is a simple, modern and secure runtime for JavaScript and
-            TypeScript that uses V8 and is built in Rust.
+            Deno bu Javascript va Typescript dasturlash tillar uchun yaratilgan,
+            sodda, zamonaviy va xavfsiz yordamchi dastur. Deno Rust dasturlash
+            tilida yaratilgan va V8 tuzilishini ishlatadi.
           </p>
           <ol className="ml-8 list-disc text-gray-700">
             <li>
-              Secure by default. No file, network, or environment access, unless
-              explicitly enabled.
+              Doimiy xavfsiz. Toki ruxsat etilmaguncha hech qanday tarmoq yoki
+              kalit so'zlar manbalariga ruxsat berilmaydi.
             </li>
-            <li>Supports TypeScript out of the box.</li>
-            <li>Ships only a single executable file.</li>
+            <li>Typescript dasturlash tilini o'z ichiga qamrab olgan.</li>
+            <li>Yagona ishga tushuriladigan fayl sifatida ishlaydi.</li>
             <li>
-              Has built-in utilities like a dependency inspector (
-              <InlineCode>deno info</InlineCode>) and a code formatter (
+              Ichidan taqdim etiladigan yordamchi instrumentlar mavjud. Misol, (
+              <InlineCode>deno info</InlineCode>) va kodni tartibga soluvchi (
               <InlineCode>deno fmt</InlineCode>).
             </li>
             <li>
-              Has a set of reviewed (audited) standard modules that are
-              guaranteed to work with Deno:{" "}
+              Deno bilan ishlashi anniq bo'lgan va tasdiqlangan standart
+              kutubxonasiga ega:{" "}
               <a href="https://deno.land/std" className="link">
                 deno.land/std
               </a>
@@ -122,19 +123,18 @@ for await (const req of s) {
         </div>
         <div className="max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8">
           <p className="my-4 text-gray-700">
-            You can find a more in depth introduction, examples, and environment
-            setup guides in{" "}
+            Yanada chuqur, to'liq va misollarga ega ma'lumotlarni{" "}
             <Link href="/manual">
-              <a className="link">the manual</a>
+              <a className="link">qo'llanma</a>{" "}
             </Link>
-            .
+             dan topishingiz mumkin.
           </p>
         </div>
         <div className="max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8 mt-20">
           <Link href="#runtime-documentation">
             <a className="hover:underline">
               <h3 className="font-bold text-xl" id="runtime-documentation">
-                Yordamchi dastur dokumentatsiya
+                Yordamchi dastur dokumentatsiyasi
               </h3>
             </a>
           </Link>
@@ -297,7 +297,7 @@ const InstallSection = () => {
   const cargo = (
     <div key="cargo" className="my-4 text-gray-700">
       <p className="py-2">
-        Build and install from source using{" "}
+        Manba orqali qurib o'rnatib oling{" "}
         <a href="https://crates.io/crates/deno" className="link">
           Cargo
         </a>
